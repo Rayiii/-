809906b53b198bc52c1adcdad1366933cd6d61a4
Rayiii <81068165+Rayiii@users.noreply.github.com>
2021-12-08 13:49:14 +0800

    Add files via upload

---
+++ 蕲遇记/pages/demo/demo.js
@@ -0,0 +1,87 @@
+// pages/demo/demo.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+    message:"wdnmd",
+    isChecked: true,
+    wuli:{
+        age:21,
+        where:"Wuhan",
+        exname:"DadyUnivercity"
+    },
+    list:[
+        {
+            id:0,
+            name:"武汉理工"
+        },
+        {
+           id:1,
+           name:"野鸡大学"
+        },
+        {
+            id:2,
+            name:"狗都不上"
+        }
+    ]
+    },
+
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
